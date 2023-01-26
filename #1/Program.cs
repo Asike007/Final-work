05c0cd198016067faec2aa971e4337e50021cd3f
Aslanbek <miz_asl_93@mail.ru>
2023-01-27 02:16:43 +0600

    The code of the program that solves the problem is written

---
+++ #1/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача: Написать программу, которая из имеющегося массива сторок формирует массив из строк, длина которых меньше или равна 3 символа.
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгритма.
+
+string[] Array = { "hello", "+32", "Almaty", ":-)", "CPU", "GitHub", "Id", "144", "15489", "-44", "54689" };
+PrintArray(FilterArr(Array));
+
+string[] FilterArr(string[] array)
+{
+    int lengthOfElem = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            lengthOfElem++;
+        }
+    }
+    string[] createArray = new string[lengthOfElem];
+    int a = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            createArray[a] = array[i];
+            a++;
+        }
+    }
+    return createArray;
+}
+
+void PrintArray(string[] arr)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+    Console.Write($" {arr[arr.Length - 1]} ]");
+}
